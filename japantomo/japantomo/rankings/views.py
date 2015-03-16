dcf1111e16824275ca87d5170c49be541e5a3790
Orgil <giga811@gmail.com>
2015-03-16 21:49:14 +0900

    Migrated layout/base.html and rankings/index.html and js css

--- japantomo/japantomo/rankings/views.py
+++ japantomo/japantomo/rankings/views.py
@@ -1,3 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 rankings = Blueprint('rankings', __name__, url_prefix='/rankings')
+
+
+@rankings.route('/')
+def index():
+
+    return render_template('rankings/index.html')
